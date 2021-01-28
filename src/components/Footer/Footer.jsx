e2fbf1d3f65ab6668127692b36cc8c93236621f6
Kijpokin <kijpokin.nga@hotmail.com>
2021-01-29 02:34:30 +0700

    refactor: refactor footer to new components

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,6 @@
+import React from 'react'
+import { CopyRight } from './Footer.styled'
+
+const Footer = () => <CopyRight> © Mackie - 2021</CopyRight>
+
+export default Footer
---
+++ src/components/Footer/Footer.styled.js
@@ -0,0 +1,11 @@
+import styled from 'styled-components'
+
+export const CopyRight = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  font-size: 75%;
+  font-weight: normal;
+  margin-bottom: 0.75rem;
+`
